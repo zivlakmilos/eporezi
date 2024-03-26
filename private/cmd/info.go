3a22ceba02c213907c9efa1cfd5db33921fff0ac
Milos Zivlak <zivlakmilos@gmail.com>
2024-03-26 21:52:57 +0100

    Prints card info

--- private/cmd/info.go
+++ private/cmd/info.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+	"github.com/zivlakmilos/eporezi/pkg/scard"
 )
 
 var infoCmd = &cobra.Command{
@@ -9,6 +12,20 @@ var infoCmd = &cobra.Command{
 	Short: "Show card inforamtions",
 	Long:  "Show informations about sertificate",
 	Run: func(cmd *cobra.Command, args []string) {
+		card := scard.NewSCard(cfg.PKCS11Module)
+		err := card.Open("")
+		if err != nil {
+			ExitWithError(err)
+		}
+		defer card.Close()
+
+		info, err := card.Info()
+		if err != nil {
+			ExitWithError(err)
+		}
+
+		fmt.Printf("Label:\t\t%s\n", info.Label)
+		fmt.Printf("Serial Number:\t%s\n", info.SerialNumber)
 	},
 }
 
